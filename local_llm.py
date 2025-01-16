f755b46aef1061c2b16bf0c5b827f68a519dfbc6
tomn <tomas.nguyen138@gmail.com>
2025-01-16 18:38:07 +0100

    chat message format

--- local_llm.py
+++ local_llm.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 
+
 class Model:
     def __init__(self, model_path):
         self.model_id = model_path
@@ -15,8 +16,8 @@ class Model:
         self.tokenizer.padding_side = "left"
 
         self.model = transformers.AutoModelForCausalLM.from_pretrained(
-            self.model_id, 
-            token=ACCESS_TOKEN, 
+            self.model_id,
+            token=ACCESS_TOKEN,
         )
 
     def set_system_prompt(self, prompt):
@@ -24,23 +25,28 @@ class Model:
 
     def __call__(self, prompts):
         prompts = prompts if isinstance(prompts, list) else [prompts]
-        sequences = [f"{self.system_prompt}\n{p}" for p in prompts]
+        messages = [
+            {"role": "system", "content": self.system_prompt},
+        ] + [
+            {"role": "user", "content": p+'\n'} for p in prompts
+        ]
 
-        inputs = self.tokenizer(sequences, return_tensors="pt", padding=True)
-        print(inputs)
+        chat = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        print(chat)
+
+        inputs = self.tokenizer(chat, return_tensors="pt", padding=True)
 
         outputs = self.model.generate(
-            **inputs, 
-            max_new_tokens=10,
+            **inputs,
+            max_new_tokens=100,
         )
 
         return self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
 
-print(ACCESS_TOKEN)
-model = Model("meta-llama/Llama-3.2-1B")
-model.set_system_prompt("")
-result = model(["Capital of France is", "The capital of Czech Republic is"])
+model = Model("meta-llama/Llama-3.2-1B-Instruct")
+model.set_system_prompt("You are a chatbot assistant helping a user with their request. Keep it short and simple.")
+result = model("Tell me a joke\n\n")
 
 for r in result:
     print(r)
