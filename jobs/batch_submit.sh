ca9fc94b682626d10656648c6c92c85e4712ad8a
Hai Phong Nguyen <xnguye28@stud.fit.vutbr.cz>
2025-03-15 13:17:09 +0100

    gitignore, jobs

--- jobs/batch_submit.sh
+++ jobs/batch_submit.sh
@@ -3,7 +3,9 @@ PARAM_FILE="params.txt"
 
 export MODEL=meta-llama/Llama-3.2-3B-Instruct
 export DATA="/storage/brno2/home/tomn/czech-statements-dataset"
-LOG_DIR=$DATA/logs
+export LOG_DIR=$DATA/logs
+export EXAMPLES_PATH=datasets/examples_demagog.json
+TO_REPLACE='$EXAMPLES_PATH,$DATA,$EVIDENCE_PATH,$STATEMENTS_PATH,$PROMPT_PATH,$ALLOWED_LABELS,$MODEL,$BATCH_SIZE,$SAMPLE_PORTION,$MAX_INDEX,$EXAMPLE_COUNT,$NAME,$EXPLANATION'
 
 rm -rf .jobs
 mkdir .jobs
