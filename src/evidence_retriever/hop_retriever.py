9d6597e20ce6e3477b84758d72ac8fc682d43add
Hai Phong Nguyen <xnguye28@stud.fit.vutbr.cz>
2025-03-30 12:45:04 +0200

    retriever bugfixes

--- src/evidence_retriever/hop_retriever.py
+++ src/evidence_retriever/hop_retriever.py
@@ -1,5 +1,10 @@
 import dspy
+import logging
+import time
 from typing import Callable
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class HopRetriever(dspy.Module):
     def __init__(self, search_func: Callable[[str, int], dict|list], num_docs=10, num_hops=4):
@@ -16,15 +21,27 @@ class HopRetriever(dspy.Module):
 
         for _ in range(self.num_hops):
             # Generate query and search for new segments
+            start = time.time()
             query = self.generate_query(výrok=statement, co_zjistit=notes, jazyk=query_lang).query
+
+            if (time.time() - start) > 1:
+                logger.info(f"Generating query took {time.time() - start} seconds")
+
+            start = time.time()
             new_segments = self.search(query, self.num_docs)
+            if (time.time() - start) > 1:
+                logger.info(f"Searching took {time.time() - start} seconds")
 
             # Update retrieved segments and texts
             retrieved_segments.extend(new_segments)
             retrieved_texts.extend([segment["text"] for segment in new_segments])
 
             # Update notes for the next iteration
+            start = time.time()
             prediction = self.append_notes(výrok=statement, co_zjistit=notes, nasbírané_texty=retrieved_texts)
+
+            if (time.time() - start) > 1:
+                logger.info(f"Appending notes took {time.time() - start} seconds")
             notes.extend(prediction.co_dalšího_zjistit)
 
         return dspy.Prediction(notes=notes, segments=list({frozenset(d.items()): d for d in retrieved_segments}.values()))
--- src/evidence_retriever/train.py
+++ src/evidence_retriever/train.py
@@ -1,24 +1,31 @@
 import bm25s
+import os
 import json
 import argparse
 import dspy
 import simplemma
+import datetime
 from dataset_manager.orm import *
 from .hop_retriever import HopRetriever
-import numpy as np
+import logging
 
 to_label = []
+# save to logs/ folder
+logging.basicConfig(filename=f'logs/retriever_{str(datetime.datetime.now()).replace(' ', '-')}.log', level=logging.INFO)
+logging.getLogger("LiteLLM").setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
 
 def stemmer(words):
     return [simplemma.lemmatize(word, "cs") for word in words]
 
 def search_bm25(query: str, k: int):
     tokens = bm25s.tokenize(query, stemmer=stemmer, show_progress=False)
-    results, scores = retriever.retrieve(tokens, k=k, n_threads=1, show_progress=False)
+    results, scores = retriever.retrieve(tokens, k=k, n_threads=10, show_progress=False)
     run = [
-        {"id": corpus[idx].id, "score": score, "text": corpus[idx].text}
+        {"id": corpus[idx].id, "score": score, "text": corpus[idx].text} # type: ignore
         for idx, score in zip(results[0], scores[0])
     ]
+
     return run
 
 def eval_metric(example, pred, trace=None):
@@ -63,10 +70,10 @@ def load_statements(dataset: Session) -> list[Statement]:
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--train", action="store_true")
-    parser.add_argument("--model-path", type=str, default="together_ai/meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo")
-    parser.add_argument("--optimized-path", type=str, default=None, help="Path to the JSON optimized prompt")
-    parser.add_argument("--output-path", type=str, default="optimized_hop.json")
-    parser.add_argument("--output-predictions", type=str, default="predicted_segments.json")
+    parser.add_argument("--model-path", type=str, default="hosted_vllm/Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4")
+    parser.add_argument("--api-base", type=str, default=None)
+    parser.add_argument("--optimized-path", type=str, default=None)
+    parser.add_argument("--output-dir", type=str, default="retriever-train-results")
 
     return parser.parse_args()
 
@@ -74,38 +81,43 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     # lm = dspy.LM(args.model_path, max_tokens=3000)
-    lm = dspy.LM("hosted_vllm/Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4", api_base="http://0.0.0.0:8000/v1")
+    lm = dspy.LM(args.model_path, api_base=args.api_base)
     dspy.configure(lm=lm, provide_traceback=True)
 
     dataset = init_db()
+    os.makedirs(args.output_dir, exist_ok=True)
 
     corpus = load_segments(dataset)
     statements = load_statements(dataset)
 
     # Index the corpus for retrieval
-    corpus_tokens = bm25s.tokenize([item.text for item in corpus], stemmer=stemmer)
+    corpus_tokens = bm25s.tokenize([item.text for item in corpus], stemmer=stemmer) # type: ignore
     retriever = bm25s.BM25(k1=0.9, b=0.4)
     retriever.index(corpus_tokens)
+    logger.info("Corpus indexed")
 
     # Build examples
     examples = [dspy.Example(statement=f"{s.statement} - {s.author}, {s.date}", relevant_segments=s.segments, statement_id=s.id).with_inputs('statement') for s in statements]
+    logger.info("Examples built")
 
     hop_retriever = HopRetriever(search_bm25)
+
     if args.optimized_path:
         print("Loading optimized model")
         hop_retriever.load(args.optimized_path)
 
     if not args.train:
-        evaluate = dspy.Evaluate(devset=examples, metric=eval_metric, num_threads=16, display_progress=True, display_table=30)
+        evaluate = dspy.Evaluate(devset=examples, metric=eval_metric, num_threads=48, display_progress=True, display_table=30)
         evaluate(hop_retriever)
     else:
         # Train the model
-        tp = dspy.MIPROv2(metric=eval_metric, auto="medium", num_threads=16, prompt_model=lm)
+        tp = dspy.MIPROv2(metric=eval_metric, auto="light", num_threads=48, prompt_model=lm)
 
         kwargs = dict(minibatch_size=40, minibatch_full_eval_steps=4, requires_permission_to_run=False)
         optimized = tp.compile(hop_retriever, trainset=examples, max_bootstrapped_demos=4, max_labeled_demos=4, **kwargs)
-        optimized.save(args.output_predictions)
 
-    with open(args.output_path, "w") as f:
+        optimized.save(os.path.join(args.output_dir, "optimized_model.pkl"))
+
+    with open(os.path.join(args.output_dir, "predicted_segments.json"), "w") as f:
         # unique predicted segment dicts
         json.dump(to_label, f, indent=2, ensure_ascii=False)
